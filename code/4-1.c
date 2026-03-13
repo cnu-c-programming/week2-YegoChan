295d17fecca6d3a82f5603ed4498e21735d6ff74
Yego Chan <136900567+YegoChan@users.noreply.github.com>
2026-03-13 11:55:36 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,27 @@
 
 int main()
 {
+    int a, b;
+    char oper;
+
+    scanf("%d %d %c", &a, &b, &oper);
+
+    switch (oper) {
+        case '+':
+            printf("%d\n", a + b);
+            break;
+        case '-':
+            printf("%d\n", a - b);
+            break;
+        case '*':
+            printf("%d\n", a * b);
+            break;
+        case '/':
+            printf("%d\n", a / b);
+            break;
+        default:
+            printf("Insert correct operator!");
+    }
 
     return 0;
 }
-
