f8887c867203a12c661e47550db1037376da1ad7
Yego Chan <136900567+YegoChan@users.noreply.github.com>
2026-03-13 11:04:01 +0900

    Update 3-2.c
    Add comparison between signed and unsigned integers with explanation.

--- code/3-2.c
+++ code/3-2.c
@@ -2,7 +2,20 @@
 
 int main()
 {
+    int a = -1;
+    unsigned int b = 1;
+
+    if (a < b) {
+        printf("a < b\n");
+    } else {
+        printf("a >= b ???\n");
+    }
+    /*
+     * C에서 int와 unsigned int의 비교는, int를 unsigned int로 캐스팅 한 뒤 실행
+     * -1의 4바이트 2진수 표현: 11111111 11111111 11111111 11111111
+     * 이는 unsigned int로 2^32 - 1
+     * 따라서 2^32 - 1 > 1
+     */
 
     return 0;
 }
-
