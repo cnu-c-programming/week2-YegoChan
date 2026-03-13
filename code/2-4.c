dc7ebd6b0be2852a21c904d46162bc73a88e73c7
Yego Chan <136900567+YegoChan@users.noreply.github.com>
2026-03-13 10:22:07 +0900

    Update 2-4.c
    Added precision tests for float and double types.

--- code/2-4.c
+++ code/2-4.c
@@ -2,7 +2,22 @@
 
 int main()
 {
+    
+    float a = 0.1 + 0.2;
+    float b = 0.3;
+
+    printf("a = %.100f\n", a);
+    printf("b = %.100f\n", b);
+    printf("a == b is %d\n", a == b);
+    // float: 유효숫자가 약 6~7자리
+
+    double c = 0.1 + 0.2;
+    double d = 0.3;
+
+    printf("c = %.100f\n", c);
+    printf("d = %.100f\n", d);
+    printf("c == d is %d\n", c == d);
+    // double: 유효숫자가 약 14~15자리
 
     return 0;
 }
-
