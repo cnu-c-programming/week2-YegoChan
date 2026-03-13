53be175aaa92006956fa1588f56193927bf3e91c
Yego Chan <136900567+YegoChan@users.noreply.github.com>
2026-03-13 12:29:19 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -3,6 +3,31 @@
 int main()
 {
 
+    int input;
+    scanf("%d", &input);
+
+    if (input <= 0) {
+        printf("%s", "Please insert Natural Number greater than 1");
+        return 0;
+    }
+    if (input == 1) {
+        printf("false");
+        return 0;
+    }
+    /*
+     * 1보다 큰 자연수를 따질거면 1도 따로 예외처리를 하지 않는 게 바람직하지만
+     * 테스트 케이스에서 1 입력시 false를 출력하는 중
+     */
+    
+
+    for (int i = 2; i * i <= input; i++) {
+        if (input % i == 0) {
+            printf("false");
+            return 0;
+        }
+    }
+
+    printf("true");
+
     return 0;
 }
-
